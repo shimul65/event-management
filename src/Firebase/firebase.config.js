380e8c28cae2080a0b0c124b191fba3ba945855f
Shimul <shimulmohammad420@gmail.com>
2023-10-09 16:08:57 +0600

    enviornment variable added and deploy to firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,13 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+const all = import.meta.env;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB1YLENW-Rf5sSJYpdxyPNfr0scKhPIC1I",
-    authDomain: "wedding-event-management-8d593.firebaseapp.com",
-    projectId: "wedding-event-management-8d593",
-    storageBucket: "wedding-event-management-8d593.appspot.com",
-    messagingSenderId: "816364481764",
-    appId: "1:816364481764:web:02121e8b91d20be8b690f1"
+    apiKey: all.VITE_APIKEY,
+    authDomain: all.VITE_AUTHDOMAIN,
+    projectId: all.VITE_PROJECTID,
+    storageBucket: all.VITE_STORAGEBUCKET,
+    messagingSenderId: all.VITE_MESSAGINGSENDERID,
+    appId: all.VITE_APPID,
 };
 
 // Initialize Firebase
